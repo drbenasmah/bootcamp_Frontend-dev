3e2b2fedd34d98e98ae3f06a7c67d5baabe6436e
drbenasmah <drbenasmah2016@gmail.com>
2023-06-12 09:54:24 +0200

    update

--- javacript-files/drum-Kit/index.js
+++ javacript-files/drum-Kit/index.js
@@ -2,5 +2,62 @@ var element = document.querySelectorAll(".drum");
 
 for(i = 0; i < element.length; i++){
   element[i].addEventListener("click", function() {
-     alert("I got clicked");} )
+
+     var buttonInnerHTML = this.innerHTML;
+
+     switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+        case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+
+          case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+            case "w":
+              var tom1 = new Audio("sounds/tom-1.mp3");
+              tom1.play();
+              break;
+
+              case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+      
+                case "w":
+                  var tom1 = new Audio("sounds/tom-1.mp3");
+                  tom1.play();
+                  break;
+     
+      default:
+        break;
+     }
+
+
+
+
+
+
+
+
+
+
+
+    //  var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+  } )
 }
+
+
+
+
+
+
+
